43abae460f804f2f8f37ac6b4c7312c20f7d390e
Vimalprasath <vimalprasath2000@gmail.com>
2025-12-04 21:47:55 +0530

    EP 2 and EP3 changes commit

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-/********* 1. Printing Hello world from React !!!   ***/
+import React from "react";
+import ReactDOM from "react-dom/client"
 
-// CreateElement will take three things h1 = tag name, attributes to the tag, value of the tag
-const heading = React.createElement("h1", {id: "heading", abc: "xyz"}, "Hello World from React ❤️!!!");
-console.log(heading)    // It will print React element object not a h1 tag
+// const heading = React.createElement("h1", {id: "heading"}, "Hello World From React!");
+
+const heading = <h1 id="heading">Hello World From React</h1>
+
+const Title = () => (
+    <h1>This is Title Component</h1>
+)
+
+const firstTitle = <span>First title element</span  >
+const titleElement = <h1> {firstTitle} This is title element</h1>;
+
+const HeadingComponent = () => {    
+    return   <div>
+        {titleElement}
+        <Title />
+        <h1>Hello World From Function Component</h1>
+    </div>
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log("root", root)   // ReactDOMRoot object
-
-root.render(heading);   // This will take the object (react element) and change it into h1 tag and render it
-
-
-
-/************  2. Nested elements with siblings
-<div id = "parent">
-    <div id="child">
-        <h1> Nested elements </h1>
-        <h2> Second element </h2>
-    </div>
-</div>
-
-*************/
-
-const heading1 = React.createElement("div", {id: "Parent"}, React.createElement("div", {id: "child"}, [React.createElement("h1", {key: "1"}, "Nested elements"), React.createElement("h2", {key: "2"}, "Second element")]));
-const root1 = ReactDOM.createRoot(document.getElementById("root"));
-root1.render(heading1);
-
-
-// 1. <link rel = "" > tag
-// 2. copy paste index.js 
+root.render(<HeadingComponent />);
